2b6613536d84f8c8ab510456834a39aa869e3a17
eodnhoj <lex.wraith@Gmail.com>
2016-07-03 14:51:49 -0400

    Cleanup and centered writing area

--- bluebook.js
+++ bluebook.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
   $('#writing').text("Welcome to BlueBook!");
   $('#writing-info').text("Nothing written!");
-  $('#wordcount').text("0");
 })
 
 $('#writing').on("input propertychange", function(){
   var text = $("#writing").val();
   var wc = text.split(/[\s+, \r+, \n+]+/).length;
-  console.log(wc - 1);
+  $('#wordcount').text((wc - 1).toString() + " words");
+
 })
