024edea7a463301e18e6fa8a41590bc8ef403fb4
eodnhoj <lex.wraith@Gmail.com>
2016-07-05 23:39:25 -0400

    navbar

--- static/js/bluebook.js
+++ static/js/bluebook.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-  $('#writing').text("Welcome to BlueBook!");
-  $('#writing-info').text("Nothing written!");
+    $('#writing').text("Welcome to BlueBook!");
+    $('#writing-info').text("Nothing written!");
 })
 
 $('#writing').on("input propertychange", function(){
-  var text = $("#writing").val();
-  var wc = text.split(/[\s+, \r+, \n+]+/).length;
-  $('#wordcount').text((wc - 1).toString() + " words");
+    var text = $("#writing").val();
+    var wc = text.split(/[\s+, \r+, \n+]+/).length;
+    $('#wordcount').text((wc - 1).toString() + " words");
 
 })
