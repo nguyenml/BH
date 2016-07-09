ac401a3fb1c2b8a532c2a5241e4f6a2a7b7adf24
eodnhoj <lex.wraith@Gmail.com>
2016-07-09 14:28:25 -0400

    models

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, DateTime, Text, Boolean
 
 
 class User(Base):
@@ -11,4 +11,16 @@ class User(Base):
     last_name = Column(String)
     email = Column(String)
     password_hash = Column(String)
+    last_login = Column(Date)
+    is_logged_in = Column(Boolean)
+
+class WebSession(Base):
+    __tablename__ = "sessions"
+
+class Documents(Base):
+    __tablename__ = "documents"
+
+class Prompts(Base):
+    __tablename__ = "prompts"
+
 
