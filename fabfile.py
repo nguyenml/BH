0b58e75b8702be3eb451987116e19b62c8dedf23
eodnhoj <lex.wraith@Gmail.com>
2016-07-10 19:05:16 -0400

    cleanup

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,29 @@
-SQL_HOSTNAME = "localhost"
+import os
+import src
+
+SQL_HOSTNAME = "devdb"
 SQL_USERNAME = "root"
 SQL_PASSWORD = "root"
 
+DEFAULTS = {"db_hostname": SQL_HOSTNAME,
+            "db_username": SQL_USERNAME,
+            "db_password": SQL_PASSWORD}
 
+REQUIRED = ["db_hostname",
+            "db_username",
+            "db_password"]
+
+def get_config():
+    config = {}
+    for elem in REQUIRED:
+        env_var = os.getenv(elem)
+        if(env_var):
+            config[elem] = env_var
+        else:
+            config[elem] = DEFAULTS[elem]
+
+    return config
+
+def start(host="0.0.0.0", port=5000):
+    from src.app import app
+    app.run(host, port)
