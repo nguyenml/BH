0c08e5ad719149d4b57cd5e4e8986f1a4b2cdc7f
Lex Wraith <lex.wraith@gmail.com>
2016-08-29 21:19:40 -0400

    Successfully connected to database

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "postgresql://bard:hop@localhost:5001/bardhop-dev")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "postgresql://bardhop-dev:admin@localhost:5432/bardhop-dev")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PORT'] = 33507 # TODO: Check if production here
 
