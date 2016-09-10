ae5bd16d5d42ecd4b156f73b1ef04994d8572c17
Mike Nguyen <Lex.Wraith@gmail.com>
2016-09-10 09:10:37 -0400

    Updated reqs and ubuntu script

--- deploy/ubuntu_install.sh
+++ deploy/ubuntu_install.sh
@@ -1,2 +1,5 @@
 # Linux Install
-sudo apt-get install postgresql-client postgresql postgresql-contrib python-dev virtualenv 
+sudo apt-get install postgresql-client postgresql postgresql-contrib python-dev virtualenv fabric
+
+# Heroku
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
