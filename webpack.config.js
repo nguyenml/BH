5be6f5877b645fe75c18f1219f2a714f1aac3aab
nguyenml <nguyenml@dukes.jmu.edu>
2016-09-18 11:40:00 -0400

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 module.exports = {
-    entry: "./src/static/js/index.js",
+    entry: "./src/static/js/index.jsx",
     output: {
         path: __dirname,
-        filename: "bundle.jsx"
+        filename: "/src/static/js/bundle.js"
     },
     module: {
         loaders: [
-          {
-    test: /\.jsx?$/,         // Match both .js and .jsx files
-    exclude: /node_modules/,
-    loader: "babel",
-    query:
-      {
-        presets:['react']
-      }
-}
-}
+            {
+                test: /\.jsx?$/, // Match both .js and .jsx files
+                exclude: /node_modules/,
+                loader: "babel",
+                query: {
+                    presets: ['react']
+                }
+            }
         ]
-  };
+    }
+};
