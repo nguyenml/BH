9151c11af8982e3320d47c872784a516955db479
nguyenml <nguyenml@dukes.jmu.edu>
2016-09-22 18:04:23 -0400

    messing with pid

--- src/static/js/bundle.js
+++ src/static/js/bundle.js
@@ -55,13 +55,14 @@
 	    componentDidMount() {
 	        this.serverRequest = $.post("/getprompts", function (result) {
 	            this.setState({ result: result });
-	            console.log(result[0]);
 	        }.bind(this));
 	    }
 
 	    render() {
-	        this.componentDidMount;
-	        console.log(this.state.result[0]);
+	        var tab = [];
+	        for (var i = 0; i < this.state.result.length; i++) {
+	            tab.push(React.createElement(PromptsFront, { prompt: this.state.result[i].text, pid: this.state.result[i].pid }));
+	        }
 	        return React.createElement(
 	            "div",
 	            { className: "dashboard_front" },
@@ -143,139 +144,7 @@
 	                React.createElement(
 	                    "div",
 	                    { className: "current" },
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[0]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[1]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[2]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[3]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[4]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[5]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    ),
-	                    React.createElement(
-	                        "div",
-	                        { className: "daily_box" },
-	                        React.createElement(
-	                            "p",
-	                            null,
-	                            this.state.result[6]
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Read"
-	                        ),
-	                        React.createElement(
-	                            "button",
-	                            { className: "btn dashboard_read" },
-	                            "Write"
-	                        )
-	                    )
+	                    tab
 	                )
 	            )
 	        );
@@ -351,7 +220,7 @@
 	                ),
 	                React.createElement("hr", null)
 	            ),
-	            React.createElement(Entry, { title: "The Second One", text: " ", author: "{{firstname}}{{lastname}}", pic: "../static/images/riff.jpg", date: "July 17, 2016" })
+	            React.createElement(Entry, { title: "The Second One", text: "         asdasdfasdfasfafasdfasdfafafas", author: "{{firstname}}{{lastname}}", pic: "../static/images/riff.jpg", date: "July 17, 2016" })
 	        );
 	    }
 	}
@@ -360,26 +229,140 @@
 	class DailyPrompt extends React.Component {
 	    constructor() {
 	        super();
+	        this.state = {
+	            prompts: [],
+	            promptChoice: 0
+	        };
+	        this.handleChoice = this.handleChoice.bind(this);
+	    }
+
+	    handleChoice() {
+	        var text = $.post("/getprompts", function (response) {
+	            this.setState({ "text": text });
+	        });
+	        console.log(this.state.text);
+	    }
+
+	    componentDidMount() {
+	        this.serverRequest = $.post("/getprompts", function (response) {
+	            this.setState({ prompts: response });
+	        }.bind(this));
 	    }
 
 	    render() {
+	        var tab = [];
+	        for (var i = 0; i < this.state.prompts.length; i++) {
+	            tab.push(React.createElement(Prompts, { prompt: this.state.prompts[i].text, pid: this.state.prompts[i].pid }));
+	        }
 	        return React.createElement(
 	            "div",
 	            null,
 	            React.createElement(
 	                "div",
 	                { className: "daily_prompt" },
-	                React.createElement(
-	                    "h1",
-	                    { className: "daily" },
-	                    "Prisoners won't try to excape if they don't even know they are in prison."
-	                ),
-	                React.createElement("hr", null)
+	                tab
 	            ),
 	            React.createElement(Entry, { title: "The Biggest One", text: "", author: "{{story}} {{lastname}}", pic: "../static/images/riff.jpg", date: "July 17, 2016" })
 	        );
 	    }
 
+	}
+
+	class Prompts extends React.Component {
+	    constructor(props) {
+	        super(props);
+	        this.prompt = props.prompt;
+	        this.state = { promptChoice: 0 };
+	        this.handleChoice = this.handleChoice.bind(this);
+	    }
+	    handleChoice() {
+	        var text = $.post("/getprompts", function (response) {
+	            this.setState({});
+	        });
+	        console.log("test");
+	    }
+
+	    render() {
+	        return React.createElement(
+	            "div",
+	            { className: "prompt_tab" },
+	            React.createElement(
+	                "div",
+	                { onClick: this.handleChoice, className: "daily_box" },
+	                React.createElement(
+	                    "p",
+	                    null,
+	                    this.prompt
+	                )
+	            )
+	        );
+	    }
+	}
+
+	class PromptsFront extends React.Component {
+	    constructor(props) {
+	        super(props);
+	        this.prompt = props.prompt;
+	        this.state = { promptChoice: 0 };
+	        this.handleChoice = this.handleChoice.bind(this);
+	    }
+	    handleChoice() {
+	        var text = $.post("/getprompts", function (response) {
+	            this.setState({});
+	        });
+	        console.log("test");
+	    }
+
+	    render() {
+	        return React.createElement(
+	            "div",
+	            { className: "daily_box" },
+	            React.createElement(
+	                "p",
+	                null,
+	                this.prompt
+	            ),
+	            React.createElement(
+	                "button",
+	                { className: "btn dashboard_read" },
+	                "Read"
+	            ),
+	            React.createElement(
+	                "button",
+	                { className: "btn dashboard_read" },
+	                "Write"
+	            )
+	        );
+	    }
+	}
+
+	class PromptsWriting extends React.Component {
+	    constructor(props) {
+	        super(props);
+	        this.prompt = props.prompt;
+	        this.pid = props.pid;
+	    }
+
+	    render() {
+	        return React.createElement(
+	            "div",
+	            { className: "promptInfo" },
+	            React.createElement(
+	                "a",
+	                { href: '/', className: "ph" },
+	                React.createElement(
+	                    "div",
+	                    { className: "p-box" },
+	                    React.createElement(
+	                        "h1",
+	                        { className: "writing_page_prompts" },
+	                        this.prompt,
+	                        this.pid
+	                    )
+	                )
+	            )
+	        );
+	    }
 	}
 
 	//Switches between the top entries and the newest entries
@@ -417,10 +400,14 @@
 	        );
 	    }
 	};
-	/////////////////////////////////////////////
+	/////////////////////////////////////////////WRITING//////////////////////////////////////////////////////////------------------------------------------------------------------------------------------------------------------------------------
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	class Writing extends React.Component {
-	    constructor() {
-	        super();
+	    constructor(props) {
+	        super(props);
+	        this.pid = props.pid;
 	    }
 
 	    render() {
@@ -450,7 +437,8 @@
 	                "section",
 	                { className: "writingpage_section" },
 	                React.createElement("article", { id: "text", contentEditable: "true", className: "content writingpage_article" })
-	            )
+	            ),
+	            React.createElement(Prompt, null)
 	        );
 	    }
 	}
@@ -466,9 +454,13 @@
 	    },
 	    render: function () {
 	        return React.createElement(
-	            "h1",
-	            null,
-	            this.props.title
+	            "div",
+	            { className: "prompt" },
+	            React.createElement(
+	                "h1",
+	                null,
+	                "ANALYSIS"
+	            )
 	        );
 	    }
 	});
@@ -483,18 +475,55 @@
 	    }
 	};
 
+	class WritingSelection extends React.Component {
+	    constructor() {
+	        super();
+	        this.state = { result: [], pid: [] };
+	        this.handleChoice = this.handleChoice.bind(this);
+	    }
+
+	    componentDidMount() {
+	        this.serverRequest = $.post("/getprompts", function (result) {
+	            this.setState({ result: result });
+	            console.log("result");
+	        }.bind(this));
+	    }
+
+	    handleChoice() {
+	        this.setState({ pid: this.state.result.pid });
+	    }
+
+	    render() {
+	        var tab = [];
+	        for (var i = 0; i < this.state.result.length; i++) {
+	            tab.push(React.createElement(PromptsWriting, { prompt: this.state.result[i].text, pid: this.state.result[i].pid }));
+	        }
+	        return React.createElement(
+	            "div",
+	            null,
+	            React.createElement(
+	                "h1",
+	                { className: "promptOfTheDay" },
+	                "PROMPTINFO"
+	            ),
+	            React.createElement(
+	                "div",
+	                { className: "selectionBox" },
+	                tab
+	            )
+	        );
+	    }
+	}
+
 	//
 	//ReactDOM.render(<Writing/>, document.getElementById('writing_page'));
 	//ReactDOM.render(<Story/>,document.getElementById('story'))
-	console.log(window.location.href.split('/'));
 	window.onload = function () {
 	    var url = window.location.href.split('/');
-	    console.log(inArray('dashboard', url));
 	    if (inArray("dashboard", url)) {
 	        ReactDOM.render(React.createElement(Front, { pic: "../static/images/lion.jpg" }), document.getElementById('d_board'));
 	    } else if (inArray("writing", url)) {
-	        ReactDOM.render(React.createElement(Writing, null), document.getElementById('writing_page'));
-	        ReactDOM.render(React.createElement(Prompt, { title: "ANALYSIS" }), document.getElementById('prompt'));
+	        ReactDOM.render(React.createElement(WritingSelection, null), document.getElementById('writing_page'));
 	    } else if (inArray("reading", url)) {
 	        ReactDOM.render(React.createElement(Story, null), document.getElementById('story'));
 	    }
