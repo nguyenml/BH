5c425a80737afe644dacd62c62369356b60fefd4
Lex Wraith <lex.wraith@gmail.com>
2016-10-17 15:29:45 -0400

    Redis added

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == "__main__":
     from src.app import app
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000), threaded=True))
--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -7,6 +8,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "postgresql://bardhop-dev:admin@localhost:5432/bardhop-dev")
+app.config['REDIS_URL'] = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/0")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PORT'] = 33507 # TODO: Check if production here
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'longviet')
@@ -16,3 +18,6 @@ app.app_context().push()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+r = redis.from_url(app.config['REDIS_URL'])
+
