fc28e6653244e6980f79b1cc42ee89df12f6dde6
Lex Wraith <lex.wraith@gmail.com>
2016-10-21 16:36:08 -0400

    Updated install script

--- deploy/ubuntu_install.sh
+++ deploy/ubuntu_install.sh
@@ -2,4 +2,10 @@
 sudo apt-get install postgresql-client postgresql postgresql-contrib python-dev virtualenv fabric
 
 # Heroku
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | s
+
+# Redis
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
