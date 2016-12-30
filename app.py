f85aea2e2e83f0cab4303ce82ac240bc45ee2389
nguyenml <nguyenml@dukes.jmu.edu>
2016-12-30 15:54:55 -0500

    app

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 from src.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000), threaded=True))
+     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
